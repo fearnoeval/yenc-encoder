54a0b8f3e22bc95fc0d7e1f3feea54575af02d97
Tim Walter <tim@decentmail.com>
2017-05-30 00:10:26 -0400

    Update string tests

--- src/test/java/com/fearnoeval/yenc/test/StringTest.java
+++ src/test/java/com/fearnoeval/yenc/test/StringTest.java
@@ -10,32 +10,32 @@ public class StringTest {
 
   @Test
   public void multiPartSubject() {
-    final byte[] result = YencEncoder.multiPartSubject("test", 0, 0, "test", 0, 0);
-    final byte[] expected = "test [0/0] - \"test\" yEnc (0/0)".getBytes(utf8);
+    final byte[] result = YencEncoder.multiPartSubject("test", 0, 1, "test", 2, 3);
+    final byte[] expected = "test [0/1] - \"test\" yEnc (2/3)".getBytes(utf8);
 
     assertArrayEquals(result, expected);
   }
 
   @Test
   public void multiPartHeader() {
-    final byte[] result   = YencEncoder.multiPartHeader(0, 0, 0, 0, "test", 0, 0);
-    final byte[] expected = "=ybegin part=0 total=0 line=0 size=0 name=test\r\n=ypart begin=0 end=0\r\n".getBytes(utf8);
+    final byte[] result   = YencEncoder.multiPartHeader(0, 1, 2, 3, "test", 4, 5);
+    final byte[] expected = "=ybegin part=0 total=1 line=2 size=3 name=test\r\n=ypart begin=4 end=5\r\n".getBytes(utf8);
 
     assertArrayEquals(result, expected);
   }
 
   @Test
   public void multiPartTrailer() {
-    final byte[] result   = YencEncoder.multiPartTrailer(0, 0, 0);
-    final byte[] expected = "=yend size=0 part=0 pcrc32=00000000\r\n.\r\n".getBytes(utf8);;
+    final byte[] result   = YencEncoder.multiPartTrailer(0, 1, 2);
+    final byte[] expected = "=yend size=0 part=1 pcrc32=00000002\r\n.\r\n".getBytes(utf8);;
 
     assertArrayEquals(result, expected);
   }
 
   @Test
   public void multiPartTrailerLast() {
-    final byte[] result   = YencEncoder.multiPartTrailerLast(0, 0, 0, 0);
-    final byte[] expected = "=yend size=0 part=0 pcrc32=00000000 crc32=00000000\r\n.\r\n".getBytes(utf8);;
+    final byte[] result   = YencEncoder.multiPartTrailerLast(0, 1, 2, 3);
+    final byte[] expected = "=yend size=0 part=1 pcrc32=00000002 crc32=00000003\r\n.\r\n".getBytes(utf8);;
 
     assertArrayEquals(result, expected);
   }
